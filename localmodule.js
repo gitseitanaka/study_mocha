72afe63a3203487d22426e78ea6af062fbd5cbdc
Hirokazu Tanaka <gitseitanaka@users.noreply.github.com>
2015-06-17 07:57:01 +0900

    * formated

--- localmodule.js
+++ localmodule.js
@@ -2,17 +2,17 @@
 // local module
 //--------------
 
-exports.helloSync = function( name ) { 
-  return ("Hello " + name); 
-}
+exports.helloSync = function (name) {
+	return ("Hello " + name);
+};
 
-exports.hello = function( name, callback ) {
+exports.hello = function (name, callback) {
     var modname = "Hello " + name;
 
-		(function( _name, _callback ) {
-		  setTimeout(function () {
-				_callback(_name);
-		  }, 500);
-		})(modname, callback);
-}
+	(function (_name, _callback) {
+		setTimeout(function () {
+			_callback(_name);
+		}, 500);
+	})(modname, callback);
+};
 
